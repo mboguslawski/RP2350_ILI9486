1fa2ac5d13b4b895dd4ab60d789f0faf893012df
Mateusz Bogusławski <mateusz.boguslawski@ibnet.pl>
2025-07-13 13:46:57 +0200

    Implement printFrame() method Improve comments

--- ili9486_16_pararrel.cc
+++ ili9486_16_pararrel.cc
@@ -10,7 +10,7 @@ ili9486_16_pararrel::ili9486_16_pararrel(const uint8_t csx, const uint8_t dcx, c
     d0(d0)
 {}
 
-void ili9486_16_pararrel::init() {
+void ili9486_16_pararrel::init(const ColorMode mode) {
     // GPIO setup
     gpio_init(csx);
     gpio_set_dir(csx, GPIO_OUT);
@@ -41,45 +41,33 @@ void ili9486_16_pararrel::init() {
 
     // Power control 1
     sendCommand(0xC0);
-    sendData(0x19); // VRH1[4:0] - Sets VREG1OUT voltage for positive gamma
-    sendData(0x1A); // VRH2[4:0] - Sets the VREG2OUT voltage for negative gamma
+    sendData(0x19);
+    sendData(0x1A);
 
     // Power control 2
     sendCommand(0xC1);
-    sendData(0x44); // BT[2:0] ([6] = 1) - Sets the factor used in the step-up circuits
-    sendData(0x00); // VC[2:0] - Sets VCI1 regulator output voltage.
+    sendData(0x45);
+    sendData(0x00);
 
     // Power Control 3 (For Normal Mode)
     sendCommand(0xC2);
-    /*
-    DCA0[2:0] DCA1[4:3]
-    
-    DCA0 - Selects the operating frequency of the step-up circuit 1/4/5 for Normal mode. The higher step-up operating
-    frequency enhances the drivability of the step-up circuit and the quality of display but increases the current consumption.
-    Adjust the frequency taking the trade-off between the display quality and the current consumption into account.
-    
-    DCA1 [2:0]: Selects the operating frequency of the step-up circuit 2/3 for Normal mode. The higher step-up operating
-    frequency enhances the drivability of the step-up circuit and the quality of display but increases the current consumption.
-    Adjust the frequency taking the trade-off between the display quality and the current consumption into account.
-    */    
     sendData(0x33);
 
     // VCOM Control
     sendCommand(0xC5);
-    sendData(0x00); // nVM[0] - When the NV memory is programmed, the nVM will be set as ‘1’ automatically. 0 -> NV memory is not programmed. 1- > NV memory is programmed
-    sendData(0x38); // VCM_REG[7:0] - Is used to set factor to generate VCOM voltage from the reference voltage VREG2OUT.
+    sendData(0x00);
+    sendData(0x28);
 
     // Frame Rate Control (In Normal Mode/Full Colors)
     sendCommand(0xB1);
-    sendData(0xA0); // FRS[7:4] DIVA[1:0] - FRS sets the frame frequency of full color normal mode. DIVA is division ratio for internal clocks when Normal mode.
-    sendData(0x11); // RTNA[4:0] - Is used to set 1H (line) period of Normal mode at CPU interface.
-
+    sendData(0xA0); // Modifying this can resolve flicering strips, see ILI9486 datasheet before edit
+    sendData(0x11);
 
     // Display Inversion Control
     sendCommand(0xB4);
-    sendData(0x02); // ZINV[4] DINV[1:0] - ZINV sets Z-inversion mode (0-disable, 1-enable). DINV sets the inversion mode
+    sendData(0x02);
 
-    // Display Function Control - for more info see datasheet
+    // Display Function Control
     sendCommand(0xB6);
     sendData(0x00);
     sendData(0x42);
@@ -91,45 +79,46 @@ void ili9486_16_pararrel::init() {
 
     // Positive gamma correction
     sendCommand(0xE0);
-    sendData(0x1F); // VP0[4:0]
-    sendData(0x25); // VP1[5:0]
-    sendData(0x22); // VP2[5:0]
-    sendData(0x0B); // VP4[3:0]
-    sendData(0x06); // VP6[4:0]
-    sendData(0x0A); // VP13[3:0]
-    sendData(0x4E); // VP20[6:0]
-    sendData(0xC6); // VP36[7:4] VP27[3:0]
-    sendData(0x39); // VP43[6:0]
-    sendData(0x00); // VP50[3:0]
-    sendData(0x00); // VP57[4:0]
-    sendData(0x00); // VP59[3:0]
-    sendData(0x00); // VP61[5:0]
-    sendData(0x00); // VP62[5:0]
-    sendData(0x00); // VP63[4:0]
+    sendData(0x1F);
+    sendData(0x25);
+    sendData(0x22);
+    sendData(0x0B);
+    sendData(0x06);
+    sendData(0x0A);
+    sendData(0x4E);
+    sendData(0xC6);
+    sendData(0x39);
+    sendData(0x00);
+    sendData(0x00);
+    sendData(0x00);
+    sendData(0x00);
+    sendData(0x00);
+    sendData(0x00);
 
     // Negative gamma correction
     sendCommand(0xE1);
-    sendData(0x1F); // VN0[4:0]
-    sendData(0x3F); // VN1[5:0]
-    sendData(0x3F); // VN2[5:0]
-    sendData(0x0F); // VN4[3:0]
-    sendData(0x1F); // RVN6[4:0]
-    sendData(0x0F); // VN13[3:0]
-    sendData(0x46); // VN20[6:0]
-    sendData(0x49); // VN36[7:4] VN27[3:0]
-    sendData(0x31); // VN43[6:0]
-    sendData(0x05); // VN50[3:0]
-    sendData(0x09); // VN57[4:0]
-    sendData(0x03); // VN59[3:0]
-    sendData(0x1C); // VN61[5:0]
-    sendData(0x1A); // VN62[5:0]
-    sendData(0x00); // VN63[4:0]
-    
-    // Pixel format 
-    sendCommand(0x3A);
-    sendData(0x55);    // 18-bit/pixel
+    sendData(0x1F);
+    sendData(0x3F);
+    sendData(0x3F);
+    sendData(0x0F);
+    sendData(0x1F);
+    sendData(0x0F);
+    sendData(0x46);
+    sendData(0x49);
+    sendData(0x31);
+    sendData(0x05);
+    sendData(0x09);
+    sendData(0x03);
+    sendData(0x1C);
+    sendData(0x1A);
+    sendData(0x00);
 
-    // Not sure what it does, found in docs as initialize sequence
+    // Pixel format. 0x66 RGB666, 0x55 RGB565
+    sendCommand(0x3A);
+    const uint8_t format = (mode == ColorMode::RGB656) ? 0x55 : 0x55; // TODO RGB666 format
+    sendData(format);
+
+    // Unknown init sequence
     sendCommand(0xF1);
     sendData(0x36);
     sendData(0x04);
@@ -139,6 +128,7 @@ void ili9486_16_pararrel::init() {
     sendData(0x0F);
     sendData(0xA4);
     sendData(0x02);
+
     sendCommand(0xF2);
     sendData(0x18);
     sendData(0xA3);
@@ -149,23 +139,28 @@ void ili9486_16_pararrel::init() {
     sendData(0xFF);
     sendData(0x32);
     sendData(0x00);
+
     sendCommand(0xF4);
     sendData(0x40);
     sendData(0x00);
     sendData(0x08);
     sendData(0x91);
     sendData(0x04);
+
     sendCommand(0xF8);
     sendData(0x21);
     sendData(0x04);
+
     sendCommand(0xF9);
     sendData(0x00);
     sendData(0x08);
 
-    sendCommand(0x11); // Sleep OUT
+    // Sleep OUT
+    sendCommand(0x11);
     sleep_ms(120);
 
-    sendCommand(0x29); // Display ON
+    // Display ON
+    sendCommand(0x29);
     sleep_ms(20);
 }
 
@@ -207,19 +202,12 @@ void ili9486_16_pararrel::setAddressWindow(uint16_t x0, uint16_t y0, uint16_t x1
     sendCommand(0x2C); // RAMWR
 }
 
-#define RGB888_TO_RGB565(r, g, b)  ( ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | ((b & 0xF8) >> 3) )
-
 void ili9486_16_pararrel::fillScreen(uint8_t red, uint8_t green, uint8_t blue) {
     setAddressWindow(0, 0, ili9486_16_pararrel::SHORT_SIDE - 1, ili9486_16_pararrel::LONG_SIDE -1);
-
-    red = red << 2;
-    green = green << 2;
-    blue = blue << 2;
     gpio_put(dcx, 1);
     gpio_put(csx, 0);
 
-    uint32_t color = 0;
-    color += RGB888_TO_RGB565(red, green, blue);
+    uint16_t color = rgb888_to_bgr565(red, green, blue);
 
     for (uint64_t i = 0; i < (uint64_t)ili9486_16_pararrel::LONG_SIDE * (uint64_t)ili9486_16_pararrel::SHORT_SIDE; i++) {
         // Without nops transfer is too fast (ili9486 limitations)
@@ -227,12 +215,33 @@ void ili9486_16_pararrel::fillScreen(uint8_t red, uint8_t green, uint8_t blue) {
         __asm volatile("nop");
         __asm volatile("nop");
         __asm volatile("nop");
+        gpio_clr_mask(0xFFFF);
         gpio_set_mask(color);
         __asm volatile("nop");
         __asm volatile("nop");
         gpio_put(wrx, 1);
         __asm volatile("nop");
         __asm volatile("nop");
+    }
+
+    gpio_put(csx, 1);
+    sendCommand(0x29); // Seems to discrad tearing effect while changing frame
+}
+
+void ili9486_16_pararrel::printFrame(uint8_t *buffer) {
+    sendCommand(0x29); // Seems to discrad tearing effect while changing frame
+    
+    setAddressWindow(0, 0, ili9486_16_pararrel::SHORT_SIDE - 1, ili9486_16_pararrel::LONG_SIDE -1);
+    gpio_put(dcx, 1);
+    gpio_put(csx, 0);
+
+    uint32_t c = (uint32_t)0;
+
+    for (uint64_t i = 0; i < (uint64_t)ili9486_16_pararrel::LONG_SIDE * (uint64_t)ili9486_16_pararrel::SHORT_SIDE; i++) {
+        gpio_put(wrx, 0);
+        uint32_t c = (uint32_t)0 + rgb888_to_bgr565(buffer[i*3+0], buffer[i*3+1], buffer[i*3+2]);
+        gpio_put_masked(0x0000FFFF, c);
+        gpio_put(wrx, 1);
     }
 
     gpio_put(csx, 1);
--- ili9486_16_pararrel.hpp
+++ ili9486_16_pararrel.hpp
@@ -1,3 +1,8 @@
+/*
+ili9486 driver for 16 data lines communication with rp2350
+optimazed for sending whole frame at once, as rp2350 has enough RAM 
+*/
+
 #pragma once
 
 #include "pico/stdlib.h"
@@ -10,18 +15,22 @@ public:
 	static constexpr uint16_t LONG_SIDE = (uint16_t)480;
 	static constexpr uint16_t SHORT_SIDE = (uint16_t)320;
 	static constexpr uint8_t DATA_PINS_NUM = (uint8_t)16;
+	
+	enum ColorMode {RGB656};
 
 	// Assign pin to functionalities, data line pins must be consecutive, d0 is pin with lowest number
 	ili9486_16_pararrel(const uint8_t csx, const uint8_t dcx, const uint8_t resx, const uint8_t wrx, const uint8_t d0);
 
-	void init();
+	void init(const ColorMode mode);
 	void fillScreen(uint8_t red, uint8_t green, uint8_t blue);
+	void printFrame(uint8_t *buffer);
 	void setAddressWindow(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1);
-
 private:
 	void sendCommand(uint8_t command); // Send command byte
 	void sendData(uint8_t data); // Send data byte
 	void dataOut(uint16_t data); // Slower then setting mask on gpio, to double ensure proper transfer (eg. during initialization)
+
+	inline uint16_t rgb888_to_bgr565(const uint8_t red, const uint8_t green, const uint8_t blue);
 
 	const uint8_t csx;
 	const uint8_t dcx;
@@ -29,3 +38,7 @@ private:
 	const uint8_t wrx;
 	const uint8_t d0; // D0-D15 are consecutive pins
 };
+
+uint16_t ili9486_16_pararrel::rgb888_to_bgr565(const uint8_t red, const uint8_t green, const uint8_t blue) {
+ 	return ((blue & 0xF8) << 8) | ((green & 0xFC) << 3) | ((red & 0xF8) >> 3);
+}
